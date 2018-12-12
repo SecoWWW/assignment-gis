73e52b60840e0680681d8d095943ed2189844f36
SecoWWW <patrik.scensny@gmail.com>
2018-12-12 21:47:42 +0100

    few new changes

--- app.py
+++ app.py
@@ -17,20 +17,44 @@ def root():
 def js():
     return send_file('js/bundle.js')
 
+@app.route('/js/mapbox.js')
+def mapbox():
+    return send_file('js/mapbox.js')
+
+@app.route('/js/ajaxcalls.js')
+def ajax():
+    return send_file('js/ajaxcalls.js')
+
+@app.route('/css/custom.css')
+def customstyle():
+    return send_file('css/custom.css')
+
+@app.route('/js/scripts.js')
+def scripts():
+    return send_file('js/scripts.js')
+
+@app.route('/css/bulma.css')
+def bulma():
+    return send_file('node_modules/bulma/css/bulma.css')
+
+@app.route('/css/leaflet.css')
+def leaflet_css():
+    return send_file('node_modules/leaflet/dist/leaflet.css')
+
+@app.route('/js/leaflet.js')
+def leaflet_js():
+    return send_file('node_modules/leaflet/dist/leaflet.js')
+
+# @app.route('/js/mapbox-gl.js')
+# def mapboxgl():
+#     return send_file('node_modules/mapbox-gl/dist/mapbox-gl.js')
+
 @app.route('/data')
 def connection():
     curr = conn.cursor()
     curr.execute("SELECT ST_AsGeoJSON(ST_Transform(way, 4326)::geography) FROM planet_osm_line LIMIT 1")
     rows = curr.fetchone()[0]
-    return rows
-    # for row in rows:
-    #     fuck += row
-    # cur.close()
-    # return fuck
-    
-
-
-
+    return rows        
 
 if __name__ == "__main__":
     app.run()
--- js/mapbox.js
+++ js/mapbox.js
@@ -1,17 +1,57 @@
 import mapboxgl from 'mapbox-gl';
+import { inherits } from 'util';
+
+var debug = null;
+var marker = null;
+
 
 document.onreadystatechange = () => {
     if (document.readyState == 'complete') {
-        mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vjb3d3dyIsImEiOiJjam90eDZiNGwxMmRpM29zMG0ycHNjdGJqIn0.emCFsI3QTgQBgQeZncQKXw';
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vjb3d3dyIsImEiOiJjanAzejdxanIwbGRmM3BwaGUwNmM2dHJtIn0.wN3XZTKDpYQiWCz46HLgdw';
         var map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v10'
-        });
-    
-        fetch('/data').then((data) => {
-            
-        });
+            style: 'mapbox://styles/mapbox/streets-v10',
+            center: [-87.6298, 41.8781], // starting position [lng, lat]
+            zoom: 10 // starting zoom
+        });        
     }
 
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([-87.6298, 41.8781])
+        .addTo(map);
+    
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();        
+        debug.style.display = 'block';
+        debug.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;                
+        
+
+        var xhr = new XMLHttpRequest();
+	    xhr.open('GET','/data');
+	    xhr.responseType = 'json';
+        xhr.setRequestHeader('Content-type', 'application/json');
+
+        xhr.addEventListener('load', function() {            
+            console.log(xhr.response)
+        });
+
+        xhr.send();
+
+    }
+    
+    marker.on('dragend', onDragEnd);
+
+    var debug = document.getElementById("debug");
+    
 }
+
+function init() {
+    console.log(debug);
+}
+
+
+  
+
 
