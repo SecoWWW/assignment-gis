0ad475daa8fbc6a45fa5cf2bf217d78add646c63
SecoWWW <patrik.scensny@gmail.com>
2018-12-12 23:09:39 +0100

    first select works

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify, request
 import psycopg2
 import json
 import os
@@ -48,13 +48,27 @@ def leaflet_js():
 # @app.route('/js/mapbox-gl.js')
 # def mapboxgl():
 #     return send_file('node_modules/mapbox-gl/dist/mapbox-gl.js')
-
-@app.route('/data')
+"SELECT ST_AsGeoJSON(ST_Transform(way, 4326)::geography) FROM planet_osm_line LIMIT 1"
+@app.route('/points')
 def connection():
+    latitude = request.args.get("latitude")
+    print(latitude)
+    longitude = request.args.get("longitude")
+    print(longitude)
     curr = conn.cursor()
-    curr.execute("SELECT ST_AsGeoJSON(ST_Transform(way, 4326)::geography) FROM planet_osm_line LIMIT 1")
-    rows = curr.fetchone()[0]
-    return rows        
+    curr.execute("SELECT json_build_object(\
+		'type', 'Feature',\
+		'geometry', ST_AsGeoJSON(ST_Transform(way, 4326)::geography),\
+		'name', name,\
+		'amenity', amenity\
+		)\
+        FROM planet_osm_point\
+        WHERE ST_DWITHIN(ST_SetSRID(ST_MakePoint(%(longitude)f, %(latitude)f), 4326)::geography,ST_Transform(way, 4326)::geography,100) \
+        AND name IS NOT NULL" % {'latitude': float(latitude), 'longitude': float(longitude)})
+    
+    rows = curr.fetchall()
+    print(rows)
+    return jsonify(rows)
 
 if __name__ == "__main__":
     app.run()
--- js/mapbox.js
+++ js/mapbox.js
@@ -3,17 +3,23 @@ import { inherits } from 'util';
 
 var debug = null;
 var marker = null;
+var map = null;
+var layers = [];
 
 
 document.onreadystatechange = () => {
     if (document.readyState == 'complete') {
         mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vjb3d3dyIsImEiOiJjanAzejdxanIwbGRmM3BwaGUwNmM2dHJtIn0.wN3XZTKDpYQiWCz46HLgdw';
-        var map = new mapboxgl.Map({
+        map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v10',
             center: [-87.6298, 41.8781], // starting position [lng, lat]
             zoom: 10 // starting zoom
-        });        
+        });      
+        
+        // fetch('/data').then((data) => {
+            
+        // });
     }
 
     var marker = new mapboxgl.Marker({
@@ -29,12 +35,29 @@ document.onreadystatechange = () => {
         
 
         var xhr = new XMLHttpRequest();
-	    xhr.open('GET','/data');
+	    xhr.open('GET','/points?longitude=' + lngLat.lng.toString() + '&latitude=' + lngLat.lat.toString());
 	    xhr.responseType = 'json';
         xhr.setRequestHeader('Content-type', 'application/json');
 
         xhr.addEventListener('load', function() {            
-            console.log(xhr.response)
+            // console.log(xhr.response)
+            var array = xhr.response  
+            // console.log(array)  
+            
+            for (var i = 0; i < layers.length; i++){
+                map.removeLayer(layers[i])
+                map.removeSource(layers[i])
+            }
+            layers = []
+            
+            for (var i = 0; i < array.length; i++){
+                console.log("Another")
+                console.log(array[i][0])
+                console.log(JSON.parse(array[i][0].geometry))
+                var localJSON = JSON.parse(array[i][0].geometry)
+                layers.push("point" + i.toString())
+                addPoint(localJSON.coordinates[0], localJSON.coordinates[1], array[i][0].name, "point" + i.toString())
+            }
         });
 
         xhr.send();
@@ -49,6 +72,37 @@ document.onreadystatechange = () => {
 
 function init() {
     console.log(debug);
+}
+
+function addPoint(lati, long, name, id) {
+    map.addLayer({
+        "id": id,
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [lati, long]
+                    },
+                    "properties": {
+                        "title": name,
+                        "icon": "circle"
+                    }
+                }]
+            }
+        },
+        "layout": {
+            "icon-image": "{icon}-15",
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+    });
 }
 
 
